0d3653b8a8bf58b1d2bf91a8038bfa5dc8b75824
ghushe <gayatrihushe@gmail.com>
2021-07-09 14:10:03 +0530

    adding updated Time

--- task4.js
+++ task4.js
@@ -1,14 +1,18 @@
 let task = {}
 var myList = []
 
+var lastUpdatedTime;
+
 window.onload = function() {
     if (!getFromAdmin())
         return;
 
     myList = JSON.parse(getFromAdmin())
     for (let index = 0; index < myList.length; index++) {
-        if (!myList[index].isDeleted)
+        if (!myList[index].isDeleted) {
+            document.getElementById('lastUpdatedTime').innerHTML = getLastUpdatedTime();
             renderElements(myList[index]);
+        }
     }
 };
 
@@ -26,10 +30,19 @@ input.addEventListener('keyup', function(e) {
 
 function storeToAdmin() {
     localStorage.setItem("myTaskList", JSON.stringify(myList));
+
+}
+
+function storeLastUpdatedTime() {
+    localStorage.setItem("lastUpdatedTime", moment().format('MMMM Do YYYY, h:mm:ss a'))
 }
 
 function getFromAdmin() {
     return localStorage.getItem("myTaskList");
+}
+
+function getLastUpdatedTime() {
+    return localStorage.getItem("lastUpdatedTime");
 }
 
 function renderElements(task) {
@@ -48,9 +61,12 @@ function renderElements(task) {
         btnDelete.remove();
         console.log(myList)
         storeToAdmin(task);
+        storeLastUpdatedTime();
+        document.getElementById('lastUpdatedTime').innerHTML = getLastUpdatedTime();
     });
     li.append(btnDelete);
     storeToAdmin();
+    document.getElementById('lastUpdatedTime').innerHTML = getLastUpdatedTime();
 }
 
 function addToList(info) {
@@ -60,9 +76,30 @@ function addToList(info) {
         createdTime: moment().format('MMMM Do YYYY, h:mm:ss a'),
         isDeleted: false,
     };
+
+
     myList.push(task);
     renderElements(task);
     storeToAdmin(myList);
+    storeLastUpdatedTime();
+    document.getElementById('lastUpdatedTime').innerHTML = getLastUpdatedTime();
     console.log(myList);
 
 }
+
+
+document.getElementById('deleteAll').addEventListener("click", function() {
+    console.log("Delete All");
+    if (!getFromAdmin())
+        return;
+
+    myList = JSON.parse(getFromAdmin())
+    for (let index = 0; index < myList.length; index++) {
+        if (!myList[index].isDeleted) {
+            myList[index].isDeleted = true;
+            storeLastUpdatedTime();
+            document.getElementById('lastUpdatedTime').innerHTML = getLastUpdatedTime();
+            //renderElements(myList[index]);
+        }
+    }
+})
